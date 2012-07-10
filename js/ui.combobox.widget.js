b5e654257bef926a5af33e6c7a53325cdd5e2c27
Bryan J. Agee <bryan@pamiris.com>
2012-07-10 03:38:02 -0700

    updates licensing to GPL / MIT

--- js/ui.combobox.widget.js
+++ js/ui.combobox.widget.js
@@ -23,18 +23,11 @@
  *
  * @author: John Kupko
  * @company: Pamiris Inc.
- * This software is licensed with the FreeBSD license: 
+ * This software is licensed under the GPL v3 and the MIT licenses:
+    http://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt
+    http://github.com/jquery/jquery/blob/master/GPL-LICENSE.txt
  
 Copyright 2012 Pamiris Inc. All rights reserved
- 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, 
-    this list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
     
 THIS SOFTWARE IS PROVIDED BY THE FREEBSD PROJECT ``AS IS'' AND ANY EXPRESS OR 
 IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
